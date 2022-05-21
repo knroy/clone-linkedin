7a8f1056124f8d34c22f5e04b70c3f7f1dfb0328
Komol Nath Roy <Roy@Komols-MacBook-Pro.local>
2022-05-22 00:11:20 +0600

    nav bar

---
+++ src/app/app-landing/app-landing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppLandingRoutingModule } from './app-landing-routing.module';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
+
+
+@NgModule({
+  declarations: [
+    LandingPageComponent,
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule,
+    AppLandingRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule
+  ]
+})
+export class AppLandingModule { }
